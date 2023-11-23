6c9d091434f081fd7825fca0f25159d2008bb0c7
ppauel <hello@pauel.eu>
2023-11-24 00:15:06 +0100

    Fix date offset

--- src/utilities/format.ts
+++ src/utilities/format.ts
@@ -54,5 +54,8 @@ export const dateNameToDate = (dateString: Dates): Date | null => {
 }
 
 export const dateToDateString = (date: Date): string => {
-    return date.toISOString().split('T')[0];
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    return `${year}-${month}-${day}`;
 }
