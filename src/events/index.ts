eb86cc74e6fb0da41131444dba2df3859113ec1e
Paul <hello@pauel.eu>
2023-12-22 12:27:01 +0100

    Update index.ts

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,4 +1,3 @@
 export { default as interactionCreate } from './interactionCreate';
 export { default as messageCreate } from './messageCreate';
-// export { default as speiseplan } from './messageReactionAdd';
 export { default as ready } from './ready';
